278670425e4a2ad054234872302264bde3c8dcd7
Blindeen <kuba.szmajda@gmail.com>
2023-11-17 09:04:44 +0100

    modify db connection

--- db-connection.php
+++ db-connection.php
@@ -1,9 +1,15 @@
 <?php
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $servername = "localhost";
 $port = 3306;
 $username = "windows";
 $password = "qwerty123";
 $database = "gym";
 
-$conn = new mysqli($servername, $username, $password, $database, $port);
+try {
+    $conn = new mysqli($servername, $username, $password, $database, $port);
+} catch (mysqli_sql_exception $exception) {
+    die("Connection failed: " . $exception->getMessage());
+}
