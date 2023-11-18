7bda6dc23e6a58c954613ba1fb07c3980730f1cb
Blindeen <kuba.szmajda@gmail.com>
2023-11-18 21:53:48 +0100

    minor changes

--- db-connection.php
+++ db-connection.php
@@ -1,15 +1,10 @@
 <?php
+require_once 'constants.php';
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$servername = "localhost";
-$port = 3306;
-$username = "windows";
-$password = "qwerty123";
-$database = "gym";
-
 try {
-    $conn = new mysqli($servername, $username, $password, $database, $port);
+    $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DATABASE, PORT);
 } catch (mysqli_sql_exception $exception) {
     die("Connection failed: " . $exception->getMessage());
 }
