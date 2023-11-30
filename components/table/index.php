b2a5d0be18f678bc1ecad56b15c51e57af408180
Blindeen <kuba.szmajda@gmail.com>
2023-11-30 21:48:01 +0100

    add generic table component

--- components/table/index.php
+++ components/table/index.php
@@ -1,41 +1,66 @@
-<table>
-    <thead>
-    <tr>
-        <th>Name</th>
-        <th>Weekday</th>
-        <th>Time</th>
-        <th>Room</th>
-        <th>Action</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    $query = "SELECT Activities.ID, Name, DayOfWeek, StartTime, EndTime, RoomNumber FROM Activities
-                INNER JOIN gym.Rooms R on Activities.RoomID = R.ID
-                ORDER BY
-                    CASE DayOfWeek
-                        WHEN 'Monday' THEN 1
-                        WHEN 'Tuesday' THEN 2
-                        WHEN 'Wednesday' THEN 3
-                        WHEN 'Thursday' THEN 4
-                        WHEN 'Friday' THEN 5
-                    END, StartTime";
+<?php
+function table(mysqli $conn, array $columns, string $query, int $page, int $per_page, string $pagination_query): void
+{
+    $lower_bound = ($page - 1) * $per_page;
+    $upper_bound = $page * $per_page;
+    $query = $query . " LIMIT $lower_bound, $upper_bound";
+
+    echo '<div id="table-wrapper">
+            <table>
+                <thead>
+                    <tr>
+    ';
+
+    foreach ($columns as $column) {
+        echo "<th>$column</th>";
+    }
+
+    echo '          </tr>
+                </thead>
+                <tbody>';
+
     $result = $conn->query($query);
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)):
-        ?>
-        <tr>
-            <td><?php echo $row['Name']; ?></td>
-            <td><?php echo $row['DayOfWeek']; ?></td>
-            <td>
-                <?php echo date('H:i', strtotime($row['StartTime'])) . ' - ' . date('H:i', strtotime($row['EndTime'])); ?>
-            </td>
-            <td><?php echo $row['RoomNumber']; ?></td>
-            <td>
-                <a class="remove-button" href="<?php echo 'delete-activity.php?id=' . $row['ID']; ?>">Delete</a>
-            </td>
-        </tr>
-    <?php
+    if (!$result->num_rows) {
+        $columns_quantity = count($columns);
+        echo '<tr>';
+            echo "<td id='empty-row' colspan='$columns_quantity'>No data available</td>";
+        echo '</tr>';
+    }
+
+    while ($row = $result->fetch_row()):
+        echo '<tr>';
+        $len = count($row);
+        for ($i = 1; $i < $len; $i++) {
+            echo "<td>$row[$i]</td>";
+        }
+        echo "<td>
+                <a class='remove-button' href='delete-activity.php?id=$row[0]'>Delete</a>
+              </td>";
+        echo '</tr>';
     endwhile;
-    ?>
-    </tbody>
-</table>
+
+    echo '      </tbody>
+            </table>
+        </div>     ';
+
+    echo '<div id="pagination-wrapper">
+               <div id="pagination">';
+
+    $result = $conn->query($pagination_query);
+    $row = $result->fetch_row();
+    $pages_quantity = ceil($row[0] / $per_page);
+    if ($page > 1) {
+        $previous_page = $page - 1;
+        echo "<a href='{$_SERVER['PHP_SELF']}?page=$previous_page'> < </a>";
+    }
+    if ($pages_quantity != 0) {
+        echo $page;
+    }
+    if ($page < $pages_quantity) {
+        $next_page = $page + 1;
+        echo "<a href='{$_SERVER['PHP_SELF']}?page=$next_page'> > </a>";
+    }
+
+    echo '    </div>
+          </div>';
+}
--- trainer-dashboard/index.php
+++ trainer-dashboard/index.php
@@ -13,40 +13,40 @@ private_route($trainer, $index);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="../assets/css/components/styles.css">
+    <link rel="stylesheet" href="../components/styles.css">
     <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico">
     <title>FitSphere - Trainer dashboard</title>
 </head>
 <body>
-<?php require_once '../header.php' ?>
+<?php require_once '../components/header/index.php'; ?>
 <main>
     <div id="dashboard">
         <div class="pane">
-            <form>
-                <h2>Your group activities</h2>
-                <div class="field">
-                    <label for="activity">Activity</label>
-                    <select id="activity" name="activity">
-                        <option selected hidden></option>
-                        <?php
-                        $result = $conn->query('SELECT * FROM Activities');
-                        while ($row = $result->fetch_array(MYSQLI_ASSOC)):
-                            ?>
-
-                            <option value="<?php echo $row['ID']; ?>">
-                                <?php echo $row['Type']; ?>
-                            </option>
-
-                        <?php
-                        endwhile;
-                        ?>
-                    </select>
-                </div>
-            </form>
+            <h2>My activities</h2>
+            <?php
+            require_once '../components/table/index.php';
+            $id = $_SESSION['id'];
+            $query = "SELECT Activities.ID, Name, DayOfWeek, TIME_FORMAT(StartTime, '%H:%i'), TIME_FORMAT(EndTime, '%H:%i'), RoomNumber FROM Activities
+                INNER JOIN gym.Rooms R on Activities.RoomID = R.ID
+                WHERE TrainerID = $id
+                ORDER BY
+                    CASE DayOfWeek
+                        WHEN 'Monday' THEN 1
+                        WHEN 'Tuesday' THEN 2
+                        WHEN 'Wednesday' THEN 3
+                        WHEN 'Thursday' THEN 4
+                        WHEN 'Friday' THEN 5
+                    END, StartTime";
+            $columns = ['Name', 'Weekday', 'Start time', 'End time', 'Room', 'Action'];
+            $page = (!isset($_GET['page']) ? 1 : intval($_GET['page']));
+            $per_page = 3;
+            $pagination_query = "SELECT COUNT(*) FROM Activities WHERE TrainerID=$id";
+            table($conn, $columns, $query, $page, $per_page, $pagination_query);
+            ?>
         </div>
-        <div class="pane">
-            <form>
-                <h2>Add new activity</h2>
+        <div id="form-wrapper">
+            <h2>Add new activity</h2>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
                 <div class="field">
                     <label for="activity-name">Activity name</label>
                     <input id="activity-name" name="activity-name" type="text" required/>
@@ -54,11 +54,11 @@ private_route($trainer, $index);
                 <div class="form-row">
                     <div class="field">
                         <label for="start-hour">Start hour</label>
-                        <input id="start-hour" name="start-hour" type="time" required/>
+                        <input id="start-hour" name="start-hour" type="time" min="06:00" max="21:00" required/>
                     </div>
                     <div class="field">
-                        <label for="start-hour">End hour</label>
-                        <input id="start-hour" name="start-hour" type="time" required/>
+                        <label for="end-hour">End hour</label>
+                        <input id="end-hour" name="end-hour" type="time" min="06:00" max="21:00" required/>
                     </div>
                 </div>
                 <div class="field">
@@ -79,7 +79,7 @@ private_route($trainer, $index);
                         ?>
                     </select>
                 </div>
-                <button>Add</button>
+                <button type="submit">Add</button>
             </form>
         </div>
     </div>
