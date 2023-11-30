284de7462d37a36cc92cd34c0d2f14ad1018ce0a
Blindeen <kuba.szmajda@gmail.com>
2023-11-30 21:54:58 +0100

    complete form to add activities

--- trainer-dashboard/index.php
+++ trainer-dashboard/index.php
@@ -61,23 +61,36 @@ private_route($trainer, $index);
                         <input id="end-hour" name="end-hour" type="time" min="06:00" max="21:00" required/>
                     </div>
                 </div>
-                <div class="field">
-                    <label for="room">Room number</label>
-                    <select id="room" name="room">
-                        <option selected hidden></option>
-                        <?php
-                        $result = $conn->query('SELECT * FROM Rooms');
-                        while ($row = $result->fetch_array(MYSQLI_ASSOC)):
+                <div class="form-row">
+                    <div class="field">
+                        <label for="weekday">Weekday</label>
+                        <select id="weekday" name="weekday">
+                            <option selected hidden></option>
+                            <option value="Monday">Monday</option>
+                            <option value="Tuesday">Tuesday</option>
+                            <option value="Wednesday">Wednesday</option>
+                            <option value="Thursday">Thursday</option>
+                            <option value="Friday">Friday</option>
+                        </select>
+                    </div>
+                    <div class="field">
+                        <label for="room">Room</label>
+                        <select id="room" name="room">
+                            <option selected hidden></option>
+                            <?php
+                            $result = $conn->query('SELECT * FROM Rooms');
+                            while ($row = $result->fetch_array(MYSQLI_ASSOC)):
+                                ?>
+
+                                <option value="<?php echo $row['ID']; ?>">
+                                    <?php echo $row['RoomNumber']; ?>
+                                </option>
+
+                            <?php
+                            endwhile;
                             ?>
-
-                            <option value="<?php echo $row['ID']; ?>">
-                                <?php echo $row['RoomNumber']; ?>
-                            </option>
-
-                        <?php
-                        endwhile;
-                        ?>
-                    </select>
+                        </select>
+                    </div>
                 </div>
                 <button type="submit">Add</button>
             </form>
