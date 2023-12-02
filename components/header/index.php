cd6fc303279e253dbb259c9bae09e21891e49fcd
Blindeen <kuba.szmajda@gmail.com>
2023-12-02 23:53:06 +0100

    add user's name to navbar

--- components/header/index.php
+++ components/header/index.php
@@ -1,8 +1,10 @@
 <?php
+$first_name = $_SESSION["first-name"];
 echo
-'<header>
-    <div id="logout">
-      <a href="/index.php">Home</a>
-      <a href="../../login/logout.php">Logout</a>
+"<header>
+    <div id='logout'>
+      <span>Hi, <span id='user-name'>$first_name</span></span>
+      <a href='/index.php'>Home</a>
+      <a href='../../login/logout.php'>Logout</a>
     </div>
-</header>';
+</header>";
