31f2b3c75cfc623f6e250b6224393d9b324699bc
Blindeen <kuba.szmajda@gmail.com>
2023-12-03 09:39:57 +0100

    style up navbar

--- components/header/index.php
+++ components/header/index.php
@@ -2,7 +2,7 @@
 $first_name = $_SESSION["first-name"];
 echo
 "<header>
-    <div id='logout'>
+    <div id='items'>
       <span>Hi, <span id='user-name'>$first_name</span></span>
       <a href='/index.php'>Home</a>
       <a href='../../login/logout.php'>Logout</a>
