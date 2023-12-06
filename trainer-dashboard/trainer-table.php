eaf018608804723c4d2fc85e8186483ae508af0c
Blindeen <kuba.szmajda@gmail.com>
2023-12-06 13:49:46 +0100

    handle page number exception

--- trainer-dashboard/trainer-table.php
+++ trainer-dashboard/trainer-table.php
@@ -24,12 +24,23 @@ if (isset($_SESSION["id"])) {
                         WHEN 'Friday' THEN 5
                     END, StartTime";
 
-    if (!is_numeric($_GET["page"]) && !is_null($_GET["page"])) {
-        exit(CONSTANTS["SERVER_ERROR_MESSAGE"]);
-    }
-    $page = intval($_GET["page"] ?? 1);
     $per_page = 3;
     $pagination_query = "SELECT COUNT(*) FROM Activities WHERE TrainerID=$id";
+    try {
+        $rows_quantity = $conn->query($pagination_query)->fetch_row()[0];
+    } catch (mysqli_sql_exception $exception) {
+        error_log($exception->getMessage());
+        exit(CONSTANTS["SERVER_ERROR_MESSAGE"]);
+    }
+    $max_range = ceil($rows_quantity / $per_page);
+    $options = array(
+        "options" => array(
+            "default" => 1,
+            "min_range" => 0,
+            "max_range" => $max_range,
+        )
+    );
+    $page = filter_var($_GET["page"], FILTER_VALIDATE_INT, $options);
 
     try {
         table($conn, $columns, $action, $query, $page, $per_page, true, $pagination_query);
