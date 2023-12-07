017ee23942ef7358f3925cc3f1a9cb1a5744243c
Blindeen <kuba.szmajda@gmail.com>
2023-12-07 21:01:48 +0100

    export form handling to separate file

--- components/modal/script.js
+++ components/modal/script.js
@@ -1,40 +1,28 @@
-const editButtons = document.querySelectorAll('.edit-button');
 const editModal = document.querySelector('#modal-background');
-const modalForm = editModal.querySelector('#modal-form');
 const modalCloseButton = editModal.querySelector('.close-button');
+
+const constants = {
+    displayNone: 'none',
+    displayFlex: 'flex',
+    dataAttribute: 'data-id',
+    actionFilePath: 'edit-activity.php?id=',
+    selectTag: 'SELECT',
+    escapeKey: 'Escape',
+};
+
 modalCloseButton.addEventListener('click', () => {
-    editModal.style.display = 'none';
-})
+    editModal.style.display = constants.displayNone;
+});
 
 editModal.addEventListener('click', (e) => {
-    if (e.target !== editModal) {
-        return;
-    }
+    if (e.target !== editModal) return;
+    editModal.style.display = constants.displayNone;
+});
 
-    editModal.style.display = 'none';
-})
-
-editButtons.forEach((editButton) => editButton.addEventListener('click', () => {
-    editModal.style.display = 'flex';
-    modalForm.action = 'edit-activity.php?id=' + editButton.getAttribute('data-id');
-
-    const actionCell = editButton.parentElement;
-    const row = Array.from(actionCell.parentElement.children);
-    const actionCellIndex = row.indexOf(actionCell);
-    const dataRow = row.slice(0, actionCellIndex)
-    const allFormElements = Array.from(modalForm.elements);
-    let formFields = allFormElements.slice(0, allFormElements.length - 1);
-    formFields = [formFields[0], formFields[3], formFields[1], formFields[2], formFields[4]];
-
-    formFields.forEach((element, idx) => {
-        const data = dataRow[idx];
-
-        if (element.tagName === 'SELECT') {
-            const selectOptions = Array.from(element.children);
-            const option = selectOptions.find((element) => element.innerText === data.innerText);
-            element.value = option.value;
-        } else {
-            element.value = data.innerText;
+addEventListener('keyup', (e) => {
+    if (editModal.style.display !== constants.displayNone) {
+        if (e.key === constants.escapeKey) {
+            editModal.style.display = constants.displayNone;
         }
-    })
-}));
+    }
+});
---
+++ trainer-dashboard/script.js
@@ -0,0 +1,39 @@
+const editButtons = document.querySelectorAll('.edit-button');
+const modalForm = editModal.querySelector('#modal-form');
+
+const configForm = (editButton) => {
+    editModal.style.display = constants.displayFlex;
+    modalForm.action = constants.actionFilePath + editButton.getAttribute(constants.dataAttribute);
+};
+
+const retrieveData = (editButton) => {
+    const actionCell = editButton.parentElement;
+    const tableRow = Array.from(actionCell.parentElement.children);
+    const dataRow = tableRow.slice(0, tableRow.indexOf(actionCell))
+    const allFormElements = Array.from(modalForm.elements);
+    let formFields = allFormElements.slice(0, allFormElements.length - 1);
+    formFields = [formFields[0], formFields[3], formFields[1], formFields[2], formFields[4]];
+
+    return {formFields, dataRow};
+};
+
+const setFormValues = (formFields, dataRow) => {
+    formFields.forEach((el, idx) => {
+        const data = dataRow[idx];
+
+        if (el.tagName === constants.selectTag) {
+            const option = Array.from(el.children).find(
+                element => element.innerText === data.innerText
+            );
+            el.value = option.value;
+        } else {
+            el.value = data.innerText;
+        }
+    })
+};
+
+editButtons.forEach((editButton) => editButton.addEventListener('click', () => {
+    configForm(editButton);
+    const {formFields, dataRow} = retrieveData(editButton);
+    setFormValues(formFields, dataRow);
+}));
