721791c9d79167348142f2a21e126265e50462c5
Blindeen <kuba.szmajda@gmail.com>
2023-12-28 17:46:03 +0100

    add page containing group activities

--- classes/index.php
+++ classes/index.php
@@ -1,18 +1,26 @@
 <?php
-?>
+require_once "../components/header/index.php";
 
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="../classes/styles.css">
+    <link rel="stylesheet" href="../components/styles.css">
     <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico">
     <title>FitSphere - Classes</title>
 </head>
 <body>
-
+<?php navbar(); ?>
+<main>
+    <div id="classes-wrapper">
+        <?php require_once "classes-table.php" ?>
+    </div>
+</main>
+<script src="scripts.js"></script>
 </body>
 </html>
 
