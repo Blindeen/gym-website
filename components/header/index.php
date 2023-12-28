31a7513e30eb5b7eb342e32cb9fdecfd3fb4869e
Blindeen <kuba.szmajda@gmail.com>
2023-12-28 14:17:56 +0100

    modify header

--- components/header/index.php
+++ components/header/index.php
@@ -1,13 +1,14 @@
 <?php
 function navbar(): void
 {
+    $id = $_SESSION["id"] ?? null;
     $first_name = $_SESSION["first-name"] ?? null;
-    echo
-    "<header>
-        <div id='items'>
-            <span>Hi, <span id='user-name'>$first_name</span></span>
-            <a href='/index.php'>Home</a>
-            <a href='../../login/logout.php'>Logout</a>
-        </div>
-    </header>";
+
+    $header = "<header><div id='items'>";
+    if ($id) $header .= "<span>Hi, <span id='user-name'>$first_name</span></span>";
+    $header .= "<a href='/index.php'>Home</a>";
+    if ($id) $header .= "<a href='../../login/logout.php'>Sign out</a>";
+    $header .= "</div></header>";
+
+    echo $header;
 }
