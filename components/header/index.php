b016ea0c20800360293d1ebd702e779133ceda02
Blindeen <kuba.szmajda@gmail.com>
2023-12-29 10:17:48 +0100

    organize styles

--- components/header/index.php
+++ components/header/index.php
@@ -4,7 +4,7 @@ function navbar(): void
     $id = $_SESSION["id"] ?? null;
     $first_name = $_SESSION["first-name"] ?? null;
 
-    $header = "<header><div id='items'>";
+    $header = "<header id='navbar-header'><div id='items'>";
     if ($id) $header .= "<span>Hi, <span id='user-name'>$first_name</span></span>";
     $header .= "<a href='/index.php'>Home</a>";
     if ($id) $header .= "<a href='../../login/logout.php'>Sign out</a>";
