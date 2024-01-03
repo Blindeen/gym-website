b5ef416f25ecf35d4eb6714fc0834424ff3409fe
Blindeen <kuba.szmajda@gmail.com>
2024-01-03 21:13:03 +0100

    change fetching and displaying data

--- client-dashboard/index.php
+++ client-dashboard/index.php
@@ -39,16 +39,27 @@ private_route($client, $index);
                                     Name,
                                     DayOfWeek,
                                     StartTime,
-                                    EndTime,
                                     FirstName,
                                     LastName FROM Activities INNER JOIN Members on Activities.TrainerID = Members.ID
                                     WHERE Activities.ID NOT IN (
-                                    SELECT ActivityID FROM ActivitiesMembers WHERE MemberID = $user_id)"
+                                    SELECT ActivityID FROM ActivitiesMembers WHERE MemberID = $user_id)
+                                    ORDER BY
+                                        CASE DayOfWeek
+                                            WHEN 'Monday' THEN 1
+                                            WHEN 'Tuesday' THEN 2
+                                            WHEN 'Wednesday' THEN 3
+                                            WHEN 'Thursday' THEN 4
+                                            WHEN 'Friday' THEN 5
+                                        END, StartTime"
                         );
                         while ($row = $result->fetch_array(MYSQLI_ASSOC)):
                             ?>
                             <option value="<?php echo $row["ID"]; ?>">
-                                <?php echo $row["Name"] . " | " . $row["DayOfWeek"] . " | " . date("H:i", strtotime($row["StartTime"])) . ' | ' . $row["FirstName"] . " " . $row["LastName"] ?>
+                                <?php
+                                $row["StartTime"] = date("H:i", strtotime($row["StartTime"]));
+                                $values = array_values(array_slice($row, 1));
+                                echo sprintf("%s | %s | %s | %s %s", ...$values);
+                                ?>
                             </option>
                         <?php endwhile; ?>
                     </select>
