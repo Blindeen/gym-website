c704ae56f47cf8389ffb3de4f59c1db7f6289708
Blindeen <kuba.szmajda@gmail.com>
2024-01-03 13:38:01 +0100

    change delete activity request url

--- trainer-dashboard/delete-activity/deleteActivity.js
+++ trainer-dashboard/delete-activity/deleteActivity.js
@@ -2,7 +2,7 @@ import {sendRequest} from "../../utils.js";
 
 const onSubmit = (removeButton) => removeButton.addEventListener('click', async () => {
     const activityID = removeButton.getAttribute('data-id');
-    const url = 'delete-activity.php?id=' + activityID;
+    const url = 'delete-activity/delete-activity.php?id=' + activityID;
     const response = await sendRequest(url);
 
     if (response.ok) {
